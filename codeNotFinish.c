06d85533d89cc4dbbf1e4e46f17050e14fa68e27
maisagarcx <115797807+maisagarcx@users.noreply.github.com>
2023-08-28 15:46:32 -0300

    codeNotFinish.c

---
+++ codeNotFinish.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define MAX 1024
+#define LIMIT 0
+void readSizeOfArray(int *sizePointer){ 
+    printf("Please, input the size of your array: ");
+    do{
+    	scanf("%d", sizePointer); 
+	}while(*sizePointer<=LIMIT||*sizePointer>MAX);
+}
+void readElementsOfArray(int yourSize, int vector[MAX]){ 
+    for(int index=0;index<yourSize;index++){ 
+        printf("Input the element vector[%d]: ", index); 
+        scanf("%d", &vector[index]); 
+    }  
+} 
+void displayElementsOfArray(int yourSize, int vector[MAX]){ 
+    printf("\nYour array is:\n"); 
+    for(int index=0;index<yourSize;index++){ 
+        printf("%d ", vector[index]); 
+    } 
+}
+void calculateNormForAny(*normPointer){
+	printf("Please choose a P-norm for your calculus: ");
+	do{
+		scanf("%d", &normPointer);
+	}while(*normPointer>=LIMIT);
+}
+void main(){
+	//this program will calculate a norm for any
+	int size, norm, array[MAX];
+	readSizeOfArray(&size);
+	readElementsOfArray(size, array);
+	displayElementsOfArray(size, array);
+	whichNorm(&norm);
+	calculateSum(size, norm, array);
+	
+}
