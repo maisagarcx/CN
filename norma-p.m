2ec0e9069e88a6ad693834f4a913371f7782ebc1
maisagarcx <115797807+maisagarcx@users.noreply.github.com>
2023-09-05 15:17:30 -0300

    Create norma-p.m

---
+++ norma-p.m
@@ -0,0 +1,21 @@
+% this is me doing some shit
+% norm-p
+
+prompt = "Input the size of array: ";
+n = input(prompt);
+vet = zeros(1, n);
+
+prompt = "Input the norm P: ";
+p = input(prompt);
+
+for i=1:1:n
+    prompt = sprintf("Input element %d: ", i);
+    vet(i) = input(prompt);
+end
+
+summy=0;
+for i=1:1:n
+    summy = summy + abs(vet(i)).^p;
+end
+
+pnorm = summy .^ (1/p);
