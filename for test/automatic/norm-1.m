1be237dee4a6ad516247dd0186dbf202bf9ce8b9
maisagarcx <115797807+maisagarcx@users.noreply.github.com>
2023-09-11 08:09:14 -0300

    norm-1.m

--- for test/automatic/norm-1.m
+++ for test/automatic/norm-1.m
@@ -1,18 +1,19 @@
-% automatic norm-1
+function summy = automatic_norm_1(vet)
+    % automatic norm-1
+    n = length(vet);
+    summy = 0;
 
-n = randi([2 10]);
-vet = randi([-10 10], 1, n);
-summy=0;
+    fprintf("The size of your array is %d \n", n);
+    disp("And your array is: ");
 
-fprintf("The size of your array is %d \n", n);
-sprintf("And your array is: ", newline);
+    for i = 1:n
+        fprintf("%d ", vet(i));
+    end
 
-for(i=1:1:n)
-    fprintf("%d ", vet(i));
+    for i = 1:n
+        summy = summy + abs(vet(i));
+    end
+
+    fprintf("\nThe norm-1 of your array is %d", summy);
+
 end
-
-for(i=1:1:n)
-    summy=summy+abs(vet(i));
-end
-
-fprintf("\nThe norm-1 of your array is %d", summy);
