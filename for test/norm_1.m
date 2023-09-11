51886293439dcebf3da23b49e601b90279654087
maisagarcx <115797807+maisagarcx@users.noreply.github.com>
2023-09-11 08:41:35 -0300

    norm_1.m

---
+++ for test/norm_1.m
@@ -0,0 +1,17 @@
+function summy = norm_1()
+    prompt = "Please input the size of your array: ";
+    n = input(prompt);
+    vet = zeros(1,n);
+    
+    for i = 1:n
+        prompt = sprintf("Input element %d: ", i);
+        vet(i) = input(prompt);
+    end
+    
+    summy=0;
+    for i=1:1:n
+    summy = summy + abs(vet(i));
+    end
+
+    %fprintf('The norm-1 of your array is %f\n', summy);
+end
