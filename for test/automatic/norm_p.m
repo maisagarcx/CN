87fd6babfcd94af90c81a567102a116a51ddf4c1
maisagarcx <115797807+maisagarcx@users.noreply.github.com>
2023-09-13 07:58:15 -0300

    norm_p.m

---
+++ for test/automatic/norm_p.m
@@ -0,0 +1,15 @@
+function norm_p = norm_p(vet, p) 
+     if p<1 
+         prompt = "The norm-p must be equal or higher then 1, try again."; 
+         error(prompt);
+     end 
+  
+     n = length(vet); 
+     summy = 0; 
+
+     for i=1:n 
+         summy = summy + abs(vet(i)).^p; 
+     end 
+  
+     norm_p = summy.^(1/p);  
+ end
