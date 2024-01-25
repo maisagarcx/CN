0ca40336d18c09e4589d27f9354aed68b9f85a6d
Maísa Garcia <115797807+maisagarcx@users.noreply.github.com>
2024-01-25 14:27:54 -0300

    intNewton.m

---
+++ Funções/intNewton.m
@@ -0,0 +1,16 @@
+function [interpolated, Dely] = intNewton(number_of_points, X, Y, value_to_interpolate)
+    for i=1:number_of_points
+        Dely(i)=Y(i);
+    end
+    % construindo as diferencas divididas
+    for k=1:number_of_points-1
+        for i=number_of_points:-1:k+1
+            Dely(i)=(Dely(i)-Dely(i-1))/(X(i)-X(i-k));
+        end
+    end
+    % avaliacao pelo processo de Horner
+    interpolated=Dely(number_of_points);
+    for i=number_of_points-1:-1:1
+        interpolated=interpolated*(value_to_interpolate-X(i))+Dely(i);
+    end
+end
