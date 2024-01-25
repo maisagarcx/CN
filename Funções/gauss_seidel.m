97138bc59e98d348ae572169c729eeea472f443e
Maísa Garcia <115797807+maisagarcx@users.noreply.github.com>
2024-01-25 14:23:54 -0300

    gauss_seidel.m

---
+++ Funções/gauss_seidel.m
@@ -0,0 +1,53 @@
+function [X, iter, error] = gauss_seidel(order, matrix, ind_vet, toler, iterMax)
+    %solve systems matrix*X=ind_vet using Gauss-Siedel
+    
+    X=zeros(order,1);
+    v=zeros(order,1);
+    
+    %construction of matrices for iterations
+    for i=1:order
+        r=1/matrix(i,i);
+        for j=1:order  
+            if i~=j
+                matrix(i,j)=matrix(i,j)*r;
+            end
+        end
+        ind_vet(i)=ind_vet(i)*r;
+        X(i)=ind_vet(i);
+    end
+    iter=0;
+    
+    %Gauss-Seidel iterations
+    while true
+        iter=iter+1;
+
+        for i=1:order
+            summy=0;
+            for j=1:order
+                if i~=j
+                    summy=summy+matrix(i,j)*X(j);
+                end
+            end
+            v(i)=X(i);
+            X(i)=ind_vet(i)-summy;
+        end
+        norm_1=0;
+        norm_2=0;
+        for i=1:order
+            if abs(X(i)-v(i))>norm_1
+                norm_1=abs(X(i)-v(i));
+            end
+            if abs(X(i))>norm_2
+                norm_2=abs(X(i));
+            end
+        end
+        difMax=norm_1/norm_2;
+        fprintf("O número de iterações é %d\nA diferença máxima é %d", iter, difMax);
+        disp(X);
+        %teste de convergencia
+        if (difMax <= toler) || (iter >= iterMax)
+            break;
+        end
+    end
+    error=difMax>=toler;
+end
