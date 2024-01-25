6616d07f2c03fedb8948fe7f8ae20d493aaeba76
Maísa Garcia <115797807+maisagarcx@users.noreply.github.com>
2024-01-25 11:41:39 -0300

    ret_subst.m

---
+++ Funções/ret_subst.m
@@ -0,0 +1,17 @@
+function X = ret_subst(upper_tri_matrix, vet_ans)
+    %aceita vetor linha ou coluna como vet_ans
+    %solves upper triangular matrix systems using UX=D
+    %UX=D, is upper_tri_matrix*X=vet_ans
+    
+    [order,~] = size(upper_tri_matrix);
+    %X=zeros(order,1); %para devolver vetor coluna
+    X=zeros(1,order); %para devolver vetor linha
+    X(order) = vet_ans(order)/upper_tri_matrix(order,order);
+    for i=order-1:-1:1
+        summy=0;
+        for j=i+1:order
+            summy=summy+upper_tri_matrix(i,j)*X(j);
+        end
+        X(i)=(vet_ans(i)-summy)/upper_tri_matrix(i,i);
+    end
+end
