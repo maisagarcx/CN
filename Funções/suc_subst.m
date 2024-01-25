186b8009692a2a89118b19f3b2c570ccedc67a4d
Maísa Garcia <115797807+maisagarcx@users.noreply.github.com>
2024-01-25 11:43:32 -0300

    suc_subst.m

---
+++ Funções/suc_subst.m
@@ -0,0 +1,17 @@
+function Y = suc_subst(order, lower_tri_matrix, vet_ans)
+    %solves lower triangular matrix systems using LX=C
+    %LX=C, is lower_tri_matrix*X=vet_ans
+    if order<1 
+         prompt = "The order must be equal or higher then 1, try again."; 
+         error(prompt);
+    end
+    Y=zeros(1,order); %just of speed
+    Y(1) = vet_ans(1)/lower_tri_matrix(1,1);
+    for i=2:order
+        summy=0;
+        for j=1:i-1
+            summy=summy+lower_tri_matrix(i,j)*Y(j);
+        end
+        Y(i)=(vet_ans(i)-summy)/lower_tri_matrix(i,i);
+    end
+end
